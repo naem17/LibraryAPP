8acb2e7127f9bb1e5a5042e09924a75fb491d567
lordgretix <mrmaster@hotmail.es>
2018-11-05 08:48:53 +0100

    hola

--- app/src/main/java/com/example/admin1/libraryapp/LibraryActivity.java
+++ app/src/main/java/com/example/admin1/libraryapp/LibraryActivity.java
@@ -9,5 +9,6 @@ public class LibraryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
+        System.out.println("Hola");
     }
 }
