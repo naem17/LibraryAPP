9635708e33a16a7445a4bd43e042ab1aa6fd30e6
goizanet <39264938+goizanet@users.noreply.github.com>
2018-11-12 08:45:38 +0100

    splash + icon
    Splash activity con el gif y arreglo de la resolucion del icon

--- app/src/main/java/com/example/admin1/libraryapp/SplashActivity.java
+++ app/src/main/java/com/example/admin1/libraryapp/SplashActivity.java
@@ -6,10 +6,20 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.view.WindowManager;
+import android.widget.ProgressBar;
+
+import com.felipecsl.gifimageview.library.GifImageView;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class SplashActivity extends Activity {
 
     private final int DURATION_SPLASH = 3000;
+    private GifImageView mGifImageView;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -17,6 +27,22 @@ public class SplashActivity extends Activity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.splash);
+
+        mGifImageView = (GifImageView)findViewById(R.id.gifImageView);
+        mProgressBar = (ProgressBar)findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(mProgressBar.VISIBLE);
+
+            //set GifImageView resources
+            try {
+                InputStream inputStream = getAssets().open("logoFinal.gif");
+                byte[] bytes = IOUtils.toByteArray(inputStream);
+                mGifImageView.setBytes(bytes);
+                mGifImageView.startAnimation();
+            }catch (IOException ex) {
+
+            }
+
+            //esperar 3 segundos (duration splash) y comenzar el main activity
 
         new Handler().postDelayed(new Runnable() {
             @Override
