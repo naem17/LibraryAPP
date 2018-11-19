758e4814d6df92bc5bae5601d177c514ad19ee2c
lordgretix <mrmaster@hotmail.es>
2018-11-19 22:27:52 +0100

    Alert Comprar

--- app/src/main/java/com/example/admin1/libraryapp/CarritoActivity.java
+++ app/src/main/java/com/example/admin1/libraryapp/CarritoActivity.java
@@ -1,17 +1,22 @@
 package com.example.admin1.libraryapp;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CarritoActivity extends AppCompatActivity{
 
     private TextView nArticulos;
     private TextView mPrecio;
+    private Button mbtnComprar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,6 +25,7 @@ public class CarritoActivity extends AppCompatActivity{
 
         nArticulos = (TextView) findViewById(R.id.numero_articulos);
         mPrecio = (TextView) findViewById(R.id.carrito_precio_total);
+        mbtnComprar = (Button) findViewById(R.id.carrito_comprar);
 
         int total=0;
         double precio=0;
@@ -33,6 +39,35 @@ public class CarritoActivity extends AppCompatActivity{
 
         nArticulos.setText(String.valueOf(total));
         mPrecio.setText(String.valueOf(precio));
+
+        mbtnComprar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(CarritoActivity.this);
+                builder.setTitle(R.string.comprar_dialog_title);
+                builder.setMessage(R.string.comprar_dialog_message);
+                builder.setCancelable(false);
+
+                builder.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), R.string.compra_realizada, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                builder.setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), R.string.compra_no_realizada, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+
+
 
     }
 
